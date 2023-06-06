0aef137050055bb56f607989128d553f7d1def46
Kristina Stout <kristinalstout@gmail.com>
2023-06-05 23:11:24 -0400

    added h1 to node

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Kristina is the champion";
+document.body.appendChild(newHeader);
